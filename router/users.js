fa2fea9b08abfb9cc277d8c185f267a7a76fb7a5
kimywann <kimywan10@gmail.com>
2024-10-02 20:56:58 +0900

    요청 처리 리팩토링

--- router/users.js
+++ router/users.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const conn = require(`../mariadb`);
+const {
+    join,
+    login, 
+    passwordReset, 
+    passwordResetRequest
+    } = require('../controller/UserController');
 
 router.use(express.json());
 
-router.post('/join', (req, res) => {
-    res.json('회원가입');
-})
+router.post('/join', join)
 
-router.post('/login', (req, res) => {
-    res.json('회원가입');
-})
+router.post('/login', login)
 
-router.post('/reset', (req, res) => {
-    res.json('비밀번호 초기화 요청');
-})
+router.post('/reset', passwordResetRequest)
 
-router.put('/reset', (req, res) => {
-    res.json('비밀번호 초기화');
-})
+router.put('/reset', passwordReset)
 
 module.exports = router
