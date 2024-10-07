42f185b6e43b6672557c50cc944a44f783851623
kimywann <Kimywan10@gmail.com>
2024-10-07 23:27:29 +0900

    style: 코드 가독성 정리

--- router/users.js
+++ router/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const conn = require(`../mariadb`);
+
 const {
     join,
     login, 
@@ -11,11 +11,8 @@ const {
 router.use(express.json());
 
 router.post('/join', join)
-
 router.post('/login', login)
-
 router.post('/reset', passwordResetRequest)
-
 router.put('/reset', passwordReset)
 
 module.exports = router
