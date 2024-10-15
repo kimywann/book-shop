af60068fd18b42b7ab467c38cf0ec313b441b11e
kimywann <Kimywan10@gmail.com>
2024-10-15 20:12:47 +0900

    feat: 주문 상세 조회 구현

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -69,8 +69,24 @@ const getOrders = async (req, res) => {
     return res.status(StatusCodes.OK).json(rows);
 };
 
-const getOrderDetail = (req, res) => {
-    res.json('주문 상세 상품 조회');
+const getOrderDetail = async (req, res) => {
+
+    const {id} = req.params;
+
+    const conn = await mariadb.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '1234',
+        database : 'Bookshop',
+        dateStrings : true
+    });
+
+    let sql = `SELECT book_id, title, author, price, quantity
+                FROM orderedBook LEFT JOIN books
+                ON orderedBook.book_id = books.id
+                WHERE order_id=?;`
+    let [rows, fields] = await conn.query(sql, [id]);
+    return res.status(StatusCodes.OK).json(rows);
 };
 
 
