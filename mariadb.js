caabf0cc6e20f7d8121644946f1c531e55861a32
kimywann <Kimywan10@gmail.com>
2024-10-15 19:59:03 +0900

    refactor: MySQL 모듈에서 async/await 제거 및 기본 연결 방식 수정

--- mariadb.js
+++ mariadb.js
@@ -2,12 +2,12 @@
 const mariadb = require('mysql2');
 
 // DB와 연결 통로 생성
-const connection = mariadb.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '1234',
-    database : 'Bookshop',
-    dateStrings : true
+    const connection = mariadb.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '1234',
+        database : 'Bookshop',
+        dateStrings : true
 });
 
 module.exports = connection;
