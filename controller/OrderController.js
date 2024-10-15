a0f24fb5d8bae056a6436940f30cd6da244fa1b8
kimywann <Kimywan10@gmail.com>
2024-10-15 19:57:57 +0900

    feat: 주문 내역 조회 기능 구현

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,63 +1,73 @@
-const conn = require(`../mariadb`);
+// const conn = require(`../mariadb`);
+const mariadb = require('mysql2/promise');
 const {StatusCodes} = require('http-status-codes');
 
 
-const order = (req, res) => {
-    const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
+const order = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '1234',
+        database : 'Bookshop',
+        dateStrings : true
+    });
 
-    let delivery_id = 3;
-    let order_id = 2;
+    const {items, delivery, totalQuantity, totalPrice, userId, firstBookTitle} = req.body;
     
+    // delivery 테이블 삽입
     let sql = "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)";
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //     if(err) {
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end(); 
-    //     }
+    let [results] = await conn.execute(sql, values);
+    let delivery_id = results.insertId;
 
-    //     delivery_id = results.insertId;
-
-    //     return res.status(StatusCodes.OK).json(results);
-    // })
-
+    // orders 테이블 삽입
     sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
             VALUES (?, ?, ?, ?, ?)`;
     values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
-    // conn.query(sql, values,
-    //     (err, results) => {
-    //     if(err) {
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end(); 
-    //     }
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId
 
-    //     order_id = results.insertId;
-    //     console.log(order_id)
-
-    //     return res.status(StatusCodes.OK).json(results);
-    // })
-
+    // items를 가지고, 장바구니에서 book_id, quantity 조회 
+    sql = 'SELECT book_id, quantity FROM cartItems WHERE id IN (?)';
+    let [orderItems, fields] = await conn.query(sql, [items]);
+    
+    // orderedBook 테이블 삽입
     sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
 
     // items .. 배열 : 요소들을 하나씩 꺼내서 (forEach문 돌려서) >
     values = [];
-    items.forEach((item) => {
+    orderItems.forEach((item) => {
         values.push([order_id, item.book_id, item.quantity]);
     })
-    conn.query(sql, [values],
-        (err, results) => {
-        if(err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end(); 
-        }
-        return res.status(StatusCodes.OK).json(results);
-    })
+    results = await conn.query(sql, [values]);
+
+    let result = await deleteCartItems(conn, items);
+    return res.status(StatusCodes.OK).json(result);
 };
 
-const getOrders = (req, res) => {
-    res.json('주문 목록 조회');
+const deleteCartItems = async (conn, items) => {
+    let sql = "DELETE FROM cartItems WHERE id IN (?)";
+
+    let result = await conn.query(sql, [items]);
+    return result;
 }
+
+const getOrders = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '1234',
+        database : 'Bookshop',
+        dateStrings : true
+    });
+
+    let sql = `SELECT orders.id, created_at, address, receiver, contact, 
+                book_title, total_quantity, total_price
+                FROM orders LEFT JOIN delivery 
+                ON orders.delivery_id = delivery.id;`
+    let [rows, fields] = await conn.query(sql);
+    return res.status(StatusCodes.OK).json(rows);
+};
 
 const getOrderDetail = (req, res) => {
     res.json('주문 상세 상품 조회');
