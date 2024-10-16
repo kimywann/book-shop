b98f0b7b3321f4b479f6a3c7ac76d498e7b0646b
kimywann <Kimywan10@gmail.com>
2024-10-16 16:54:18 +0900

    feat: JWT 인증 확인 함수 ensureAuthorization 추가

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,52 +1,79 @@
 const conn = require(`../mariadb`);
 const {StatusCodes} = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const addToCart = (req, res) => {
 
     const {book_id, quantity, user_id} = req.body;
 
-    let sql = "INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)";
-    let values = [book_id, quantity, user_id];
+    let authorization = ensureAuthorization(req, res);
 
-    conn.query(sql, values,
-        (err, results) => {
-        if(err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end(); 
-        }
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 세션이 만료 되었습니다. 다시 로그인 하세요."
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰입니다."
+        });
+    } else {
+        let sql = "INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)";
+        let values = [book_id, quantity, authorization.id];
 
-        return res.status(StatusCodes.OK).json(results);
-    })
+        conn.query(sql, values,
+            (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); 
+            }
+
+            return res.status(StatusCodes.OK).json(results);
+        })
+    }
 }
 
 // 장바구니 아이템 목록 조회
 const getCartItems = (req, res) => {
-    const {user_id, selected} = req.body;
+    const {selected} = req.body;
 
-    let sql =  `SELECT cartItems.id, book_id, title, summary, quantity, price 
+    let authorization = ensureAuthorization(req, res);
+
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 세션이 만료 되었습니다. 다시 로그인 하세요."
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰입니다."
+        });
+    } else {
+        let sql =  `SELECT cartItems.id, book_id, title, summary, quantity, price 
                 FROM cartItems LEFT JOIN books 
                 ON cartItems.book_id = books.id
                 WHERE user_id=? AND cartItems.id IN (?)`;
     
-    let values = [user_id, selected]
-    conn.query(sql, values,
-        (err, results) => {
-        if(err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end(); 
-        }
+        let values = [authorization.id, selected]
+        conn.query(sql, values,
+            (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); 
+            }
 
-        return res.status(StatusCodes.OK).json(results);
-    })
+            return res.status(StatusCodes.OK).json(results);
+        })
+    }
 }
 
 // 장바구니 아이템 삭제
 const removeCartItem = (req, res) => {
 
-    const {id} = req.params; // cartItemId
+    const cartItemId = req.params.id; // cartItemId
 
     let sql = "DELETE FROM cartItems WHERE id = ?";
-    conn.query(sql, id,
+    conn.query(sql, cartItemId,
         (err, results) => {
         if(err) {
             console.log(err);
@@ -55,6 +82,24 @@ const removeCartItem = (req, res) => {
 
         return res.status(StatusCodes.OK).json(results);
     })
+}
+
+function ensureAuthorization(req, res) {
+
+    try {
+        let receivedJwt = req.headers["authorization"];
+        console.log("received jwt : ", receivedJwt);
+
+        let decodedJwt = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+        console.log(decodedJwt);
+
+        return decodedJwt;   
+    } catch (err) {
+        console.log(err.name);
+        console.log(err.message);
+
+        return err;
+    }
 }
 
 module.exports = {    
